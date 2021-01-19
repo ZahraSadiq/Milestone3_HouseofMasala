7919fe8ae3487a5b1f8c235b9145f8be81111fbc
ZahraSadiq <zsadiq27@gmail.com>
2021-01-19 23:11:56 +0000

    Create app in Heroku

--- env.py
+++ env.py
@@ -3,6 +3,6 @@ import os
 os.environ.setdefault("IP", "0.0.0.0")
 os.environ.setdefault("PORT", "5000")
 os.environ.setdefault["SECRET_KEY", "Coding2021"]
-os.environ.setdefault["MONGO_URI"] = "mongodb+srv://zahrasadiq:CodingMS32021@recipe_book.vcalk.mongodb.net/users?retryWrites=true&w=majority"
+os.environ.setdefault["MONGO_URI", ""] 
 os.environ.setdefault["MONGO_DB", "recipe_book"]
 
