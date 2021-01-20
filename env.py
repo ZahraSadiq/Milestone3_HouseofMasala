5a87f6780d9cb836a24ea0431712e42c5dc0d409
ZahraSadiq <zsadiq27@gmail.com>
2021-01-20 00:06:51 +0000

    Clean up ignored files

--- env.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault["SECRET_KEY", "Coding2021"]
-os.environ.setdefault["MONGO_URI", ""] 
-os.environ.setdefault["MONGO_DB", "recipe_book"]
-
